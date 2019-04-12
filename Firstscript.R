48dd66c1fe1a9158be9f5c1e654d63337d35ef98
youssef <youssef.lakdime@gmail.com>
2019-04-12 11:19:47 +0200

    pseudocode

---
+++ Firstscript.R
@@ -0,0 +1,16 @@
+#---------------------------------VrisBi-------------------------------#
+
+# pseudocode:
+# in verkopensheet: check per klantcode (alleen bij groothandel) hoeveel er zijn verkocht
+# genereer een random getal die het aantal daadwerkelijke verkopen moet voorstellen
+# koppel klantcode van VERKOPEN.xsls aan klantcode van CUSTOMERS.xsls en check de WINKELNAAM in CUSTOMERS.xsls
+# vergelijk het aantal verkopen met de kassaverkopen per WINKELNAAM
+# 
+#
+#
+#
+#
+#
+#
+#
+#
